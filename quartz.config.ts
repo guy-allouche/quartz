9f00da265b91e9a6a7a881c546f91bc3770fcd22
guy-allouche <guy.allouche.1@gmail.com>
2023-09-09 19:30:51 -0400

    Quartz sync: Sep 9, 2023, 7:30 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -9,7 +9,7 @@ const config: QuartzConfig = {
     analytics: {
       provider: "plausible",
     },
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "quartz.tnt.xyz",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     theme: {
